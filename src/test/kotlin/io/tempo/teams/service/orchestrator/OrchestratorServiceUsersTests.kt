bf5413b0bca20341a00d18d56ff1f7b38d384e16
Leandro Melo de Sales <leandromsales@gmail.com>
2022-09-09 07:02:59 -0300

    :white_check_mark: Add unit test structure

---
+++ src/test/kotlin/io/tempo/teams/service/orchestrator/OrchestratorServiceUsersTests.kt
@@ -0,0 +1,24 @@
+package io.tempo.teams.service.orchestrator
+
+import io.tempo.teams.service.core.exceptions.TempoException
+import io.tempo.teams.service.orchestrator.users.UsersService
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrchestratorServiceUsersTests {
+
+//    @Rule
+//    public final ExpectedException exception = ExpectedException.none();
+
+    @Autowired
+    private lateinit var usersService: UsersService
+
+    @Test
+    fun getUserNotFound() {
+        Assertions.assertThatThrownBy { usersService.get("wrong-id") }.isInstanceOf(TempoException::class.java)
+    }
+
+}
