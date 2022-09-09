157eba99e9e1cea102049c70c8d3c2e5def81a6b
Leandro Melo de Sales <leandromsales@gmail.com>
2022-09-09 04:49:34 -0300

    :coffin: Remove TeamUser approach due to RolesTeamsUsers

--- src/main/kotlin/io/tempo/teams/service/orchestrator/teams/repositories/TeamsUsersRepository.kt
+++
@@ -1,9 +0,0 @@
-package io.tempo.teams.service.orchestrator.teams.repositories
-
-import io.tempo.teams.service.orchestrator.teams.models.TeamsUsers
-import io.tempo.teams.service.orchestrator.teams.models.TeamsUsersId
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface TeamsUsersRepository : JpaRepository<TeamsUsers, TeamsUsersId> {
-
-}
